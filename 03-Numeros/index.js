264f8e243f0f360bbeede0874cc3716d93feb243
Edwincala <86900578+Edwincala@users.noreply.github.com>
2023-04-25 17:53:48 -0500

    ejercicio con variables numéricas en JS

---
+++ 03-Numeros/index.js
@@ -0,0 +1,19 @@
+let alturaCm = 170;
+
+let alturaM = 1.7;
+
+let peso = 67.5;
+
+let alturaRound = alturaM.toFixed(0);
+
+console.log(alturaRound);
+
+let pesoRound = parseInt(peso);
+
+console.log(pesoRound);
+
+let max_valor_JS = Number.MAX_VALUE;
+
+let comparacion = max_valor_JS + 1 === max_valor_JS;
+
+console.log(comparacion);

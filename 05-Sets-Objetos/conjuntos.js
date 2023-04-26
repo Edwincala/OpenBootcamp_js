0bd3a7078c5252ceb5d2418f4997fd1a944b2df9
Edwincala <86900578+Edwincala@users.noreply.github.com>
2023-04-25 22:24:44 -0500

    Ejercicio añadiendo elementos a conjuntos

---
+++ 05-Sets-Objetos/conjuntos.js
@@ -0,0 +1,9 @@
+let miLista = ["Luz", "Leonardo", "Ginneth", "Edwin"];
+
+let miSet = new Set(miLista);
+console.log(miSet);
+
+console.log(miSet.add("Edwin"));
+
+miSet.add("Javascript");
+console.log(miSet);

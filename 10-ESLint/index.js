fd6972fc664b10d5a3ddd16a8fbcf210ef450bee
Edwincala <86900578+Edwincala@users.noreply.github.com>
2023-04-27 17:59:49 -0500

    Creación del archivo de configuración de ESLint

---
+++ 10-ESLint/index.js
@@ -0,0 +1,38 @@
+let nombre = "Edwin";
+
+let apellido = "Cala";
+
+let estudiante = nombre.concat(" ", apellido);
+
+console.log(`${nombre} ${apellido}`);
+
+console.log(estudiante);
+
+let estudianteMayus = estudiante.toUpperCase();
+
+console.log(estudianteMayus);
+
+let estudianteMinus = estudiante.toLowerCase();
+
+console.log(estudianteMinus);
+
+const estudianteLength = estudiante.length;
+
+console.log(estudianteLength);
+
+let primeraLetra = nombre[0];
+
+console.log(primeraLetra);
+
+let ultimaLetra = apellido[apellido.length - 1];
+
+console.log(ultimaLetra);
+
+// regex /\s para buscar los espacios en el string
+let estudianteSinEspacios = estudiante.replace(/\s/g, "");
+
+console.log(estudianteSinEspacios);
+
+let nombreIncluido = estudiante.includes(nombre);
+
+console.log(nombreIncluido);
